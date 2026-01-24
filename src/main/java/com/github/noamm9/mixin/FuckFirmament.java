da4a2b2e2a0784b4fa5e93594a808190ec20f8e9
Noamm <belkunsama@gmail.com>
2026-01-24 22:21:41 +0200

    Add mixins to disable Firmament ModAnnouncer

---
+++ src/main/java/com/github/noamm9/mixin/FuckFirmament.java
@@ -0,0 +1,21 @@
+package com.github.noamm9.mixin;
+
+import com.github.noamm9.NoammAddons;
+import org.spongepowered.asm.mixin.Dynamic;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Pseudo
+@Mixin(targets = "moe.nea.firmament.features.misc.ModAnnouncer", remap = false)
+public class FuckFirmament {
+    @Dynamic
+    @Inject(method = "onServerJoin", at = @At("HEAD"), cancellable = true)
+    private void stopFeddingEveryone(Object event, CallbackInfo ci) {
+        ci.cancel();
+        NoammAddons.INSTANCE.getLogger().info("FUCK FIRMAMENT");
+    }
+}
+
