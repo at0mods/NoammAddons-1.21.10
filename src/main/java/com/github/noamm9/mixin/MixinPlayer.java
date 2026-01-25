6a9e9bb91357db25213c22c7d392f4c3531ce746
Noamm <belkunsama@gmail.com>
2026-01-25 18:57:14 +0200

    FEAT: ArrowFix

---
+++ src/main/java/com/github/noamm9/mixin/MixinPlayer.java
@@ -0,0 +1,31 @@
+package com.github.noamm9.mixin;
+
+import com.github.noamm9.NoammAddons;
+import com.github.noamm9.features.impl.tweaks.ArrowFix;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Player.class)
+public abstract class MixinPlayer extends LivingEntity {
+    protected MixinPlayer(EntityType<? extends LivingEntity> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void fixPullBack(CallbackInfo ci) {
+        if (!ArrowFix.INSTANCE.enabled) return;
+        if ((Player) (Object) this != NoammAddons.mc.player) return;
+        if (useItem.isEmpty()) return;
+        if (ArrowFix.isShortbow(useItem)) {
+            useItem = ItemStack.EMPTY;
+            useItemRemaining = 0;
+        }
+    }
+}
---
+++ src/main/kotlin/com/github/noamm9/features/impl/tweaks/ArrowFix.kt
@@ -0,0 +1,30 @@
+package com.github.noamm9.features.impl.tweaks
+
+import com.github.noamm9.features.Feature
+import com.github.noamm9.utils.items.ItemUtils.skyblockId
+import net.minecraft.core.component.DataComponents
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
+
+object ArrowFix: Feature("Disables Bow Pullback on ShortBows.") {
+    private val bowCache = mutableSetOf<String>()
+
+    @JvmStatic
+    fun isShortbow(item: ItemStack?): Boolean {
+        if (item == null || item.isEmpty) return false
+        if (! item.`is`(Items.BOW)) return false
+        if (item.skyblockId in bowCache) return true
+        if (! item.has(DataComponents.LORE)) return false
+        val lore = item.get(DataComponents.LORE) ?: return false
+
+        for (lineComponent in lore.lines()) {
+            val lineText = lineComponent.string
+            if (lineText.contains("Shortbow: Instantly shoots!")) {
+                bowCache.add(item.skyblockId)
+                return true
+            }
+        }
+
+        return false
+    }
+}
