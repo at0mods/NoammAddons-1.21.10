1106d5b415b955d371dbb079f3ca11c3572d2daf
Noamm <belkunsama@gmail.com>
2026-02-01 00:39:33 +0200

    FEAT: Ragnarock

---
+++ src/main/kotlin/com/github/noamm9/features/impl/dungeon/Ragnarock.kt
@@ -0,0 +1,76 @@
+package com.github.noamm9.features.impl.dungeon
+
+import com.github.noamm9.event.impl.ChatMessageEvent
+import com.github.noamm9.event.impl.MainThreadPacketReceivedEvent
+import com.github.noamm9.event.impl.TickEvent
+import com.github.noamm9.event.impl.WorldChangeEvent
+import com.github.noamm9.features.Feature
+import com.github.noamm9.ui.clickgui.componnents.getValue
+import com.github.noamm9.ui.clickgui.componnents.impl.ToggleSetting
+import com.github.noamm9.ui.clickgui.componnents.provideDelegate
+import com.github.noamm9.utils.ChatUtils
+import com.github.noamm9.utils.ChatUtils.removeFormatting
+import com.github.noamm9.utils.SoundUtils
+import com.github.noamm9.utils.ThreadUtils
+import com.github.noamm9.utils.items.ItemUtils.lore
+import com.github.noamm9.utils.items.ItemUtils.skyblockId
+import com.github.noamm9.utils.location.LocationUtils
+import net.minecraft.network.protocol.game.ClientboundSoundPacket
+import net.minecraft.sounds.SoundEvents
+import kotlin.math.roundToInt
+
+object Ragnarock: Feature("Ragnarock alerts") {
+    private val alertCancelled by ToggleSetting("Alert Cancelled", true)
+    private val strengthGainedMessage by ToggleSetting("Strength Gained", true)
+    private val m7Alert by ToggleSetting("M7 Dragon Alert")
+
+    private val cancelRegex = Regex("Ragnarock was cancelled due to (?:being hit|taking damage)!")
+    private val strengthRegex = Regex("Strength: \\+(\\d+)")
+
+    private var startTicks = 0
+
+    private val soundSequence = listOf(
+        0L to 1.22f, 120L to 1.13f, 240L to 1.29f,
+        400L to 1.60f, 520L to 1.60f, 640L to 1.72f,
+        780L to 1.89f
+    )
+
+    override fun init() {
+        register<MainThreadPacketReceivedEvent.Pre> {
+            if (! strengthGainedMessage.value) return@register
+            if (event.packet !is ClientboundSoundPacket) return@register
+            if (event.packet.sound.value().location().path != "entity.wolf.death") return@register
+            if (event.packet.pitch.toDouble() == 1.4920635) return@register
+            val item = mc.player?.mainHandItem ?: return@register
+            if (item.skyblockId != "RAGNAROCK_AXE") return@register
+            val strengthLine = item.lore.map { it.removeFormatting() }.find { it.startsWith("Strength:") } ?: return@register
+            val match = strengthRegex.find(strengthLine) ?: return@register
+            val baseStrength = match.groupValues[1].toIntOrNull() ?: return@register
+            ChatUtils.modMessage("&fGained strength: &c${(baseStrength * 1.5).roundToInt()}")
+        }
+
+        register<ChatMessageEvent> {
+            val msg = event.unformattedText
+
+            if (m7Alert.value && LocationUtils.F7Phase == 5 && msg == "[BOSS] Wither King: You... again?")
+                startTicks = 36
+            else if (alertCancelled.value && msg.matches(cancelRegex)) {
+                ChatUtils.showTitle(subtitle = "&cRagnarock Cancelled")
+                SoundUtils.playEvent(SoundEvents.NOTE_BLOCK_PLING, 0.3f)
+            }
+        }
+
+        register<TickEvent.Server> {
+            if (startTicks <= 0) return@register
+            startTicks --
+            if (startTicks != 0) return@register
+
+            ChatUtils.showTitle("rag")
+            for ((delay, pitch) in soundSequence) ThreadUtils.setTimeout(delay) {
+                SoundUtils.playEvent(SoundEvents.NOTE_BLOCK_HARP, 0.3f, pitch)
+            }
+        }
+
+        register<WorldChangeEvent> { startTicks = 0 }
+    }
+}
