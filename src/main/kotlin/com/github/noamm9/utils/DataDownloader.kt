ac6668318bf04e38dbf61d5869c143d6243d26d7
Noamm <belkunsama@gmail.com>
2026-02-01 18:00:22 +0200

    DataDownloader: use my backend instead of github api

--- src/main/kotlin/com/github/noamm9/utils/DataDownloader.kt
+++ src/main/kotlin/com/github/noamm9/utils/DataDownloader.kt
@@ -17,11 +17,8 @@ import kotlin.concurrent.thread
 import kotlin.io.path.*
 
 object DataDownloader {
-    private const val DOWNLOAD_URL = "https://github.com/Noamm9/NoammAddons-1.21.10/archive/refs/heads/data.zip"
-    private const val RAW_URL = "https://raw.githubusercontent.com/Noamm9/NoammAddons-1.21.10/refs/heads/data/"
-    private const val GITHUB_API_URL = "https://api.github.com/repos/Noamm9/NoammAddons-1.21.10/commits/data"
-
-    private val SHA_REGEX = Regex(""""sha"\s*:\s*"([^"]+)"""")
+    private const val DOWNLOAD_URL = "https://api.noammaddons.workers.dev/repo?type=zip"
+    private const val HASH_URL = "https://api.noammaddons.workers.dev/repo?type=hash"
     val LOGGER = LoggerFactory.getLogger("$MOD_NAME/DataDownloader")
 
     private val modDataPath: Path = FabricLoader.getInstance().configDir.resolve(MOD_ID).resolve("data").also {
@@ -43,8 +40,7 @@ object DataDownloader {
                 }
                 else LOGGER.info("Data is up-to-date (Version: $localHash).")
 
-            }
-            catch (e: Exception) {
+            } catch (e: Exception) {
                 LOGGER.error("Failed to check for data updates", e)
             }
         }
@@ -67,25 +63,22 @@ object DataDownloader {
             tempZipFile.deleteIfExists()
 
             LOGGER.info("Data update successful.")
-        }
-        catch (e: IOException) {
+        } catch (e: IOException) {
             LOGGER.error("Failed to update data files", e)
         }
     }
 
     private fun getRemoteCommitHash(): String? {
         return try {
-            val connection = URL(GITHUB_API_URL).openConnection() as HttpURLConnection
+            val connection = URL(HASH_URL).openConnection() as HttpURLConnection
             connection.setRequestProperty("User-Agent", "Minecraft-Mod-$MOD_ID")
             connection.requestMethod = "GET"
 
             if (connection.responseCode == 200) {
-                val response = connection.inputStream.bufferedReader().use { it.readText() }
-                SHA_REGEX.find(response)?.groupValues?.get(1)
+                connection.inputStream.bufferedReader().use { it.readText() }
             }
             else null
-        }
-        catch (e: IOException) {
+        } catch (e: IOException) {
             null
         }
     }
@@ -126,11 +119,13 @@ object DataDownloader {
 
     fun getReader(fileName: String): BufferedReader {
         val localFile = modDataPath.resolve(fileName)
-        if (localFile.exists()) return localFile.bufferedReader()
+        return if (localFile.exists()) localFile.bufferedReader()
+        else TODO("Implement remote file fetching")
 
+        /*
         LOGGER.warn("Local file '$fileName' missing. Fetching from RAW URL.")
         val connection = URL(RAW_URL + fileName).openConnection() as HttpURLConnection
         connection.requestMethod = "GET"
-        return connection.inputStream.bufferedReader()
+        return connection.inputStream.bufferedReader()*/
     }
 }
